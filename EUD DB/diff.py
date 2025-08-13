46a2393d0079ad40ef8376f5b319a905a53d259f
4Tel <kyako95@gmail.com>
2025-08-14 04:16:16 +0900

    total euddb

--- EUD DB/diff.py
+++ EUD DB/diff.py
@@ -9,6 +9,7 @@ with open("farty1billion_20221204162107.csv", "r", encoding="utf-8") as f:
     data2 = list(reader)
 
 offset1 = {row[0]: row for row in data1}
+print(len(data1), len(offset1))
 offset2 = {row[0]: row for row in data2}
 
 for offset in offset1.keys():
@@ -22,8 +23,29 @@ for offset in offset2.keys():
 for offset in offset2.keys():
     if offset in offset1:
         # name, size, length, description
-        for x in range(3, 6):
+        for x in range(3, 7):
             if offset1[offset][x - 1] != offset2[offset][x]:
                 print(
                     f"Offset {offset} differs in column {x}: {offset1[offset][x - 1]} vs {offset2[offset][x]}"
                 )
+
+
+offsetS = offset1.keys() | offset2.keys()
+
+
+with open("boratw.csv", "r", encoding="utf-8") as f:
+    reader = csv.reader(f)
+    datas = list(reader)
+offset3 = {row[-1]: row for row in datas}
+for offset in offset3.keys():
+    offset = offset[2:].zfill(8)
+    if offset not in offsetS:
+        print(f"Offset {offset} is only in boratw.csv")
+
+with open("edac31433.csv", "r", encoding="utf-8") as f:
+    reader = csv.reader(f)
+    datas = list(reader)
+offset4 = {row[0]: row for row in datas}
+for offset in offset4.keys():
+    if offset not in offsetS:
+        print(f"Offset {offset} is only in edac31433.csv")
